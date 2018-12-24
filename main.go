51a12a28914967ffc2287349d70943e828d1c093
jimmy <aiwei18@gmail.com>
2018-12-24 08:11:53 +0000

    222

--- main.go
+++ main.go
@@ -4,15 +4,6 @@ func main()  {
 	fmt.Println("hello, jenkins")
 	fmt.Println("should work")
 	fmt.Println("come on baby")
-	fmt.Println("come on baby 11")
-	fmt.Println("come on baby 12")
-	fmt.Println("come on baby 14")
-	fmt.Println("come on baby 15")
-	fmt.Println("come on baby 16")
-	fmt.Println("come on baby 17")
-	fmt.Println("come on baby 18")
-	fmt.Println("come on baby 19")
-	fmt.Println("come on baby 20")
-	fmt.Println("come on shit 7")
+	fmt.Println("failure 2")
 }
 
